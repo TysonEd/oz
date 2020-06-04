36ddcc7a9d638632216ff57ecd3b7430f6f9406a
tyso <contact@tysonedwards.com>
2020-06-04 16:58:03 +0100

    asd

--- functions.php
+++ functions.php
@@ -181,11 +181,3 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-add_action( 'after_setup_theme', 'oz_css' );
-
-function oz_css(){
-
-	add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added
-	add_editor_style( 'style-editor.css' ); // tries to include style-editor.css directly from your theme folder
-
-}
